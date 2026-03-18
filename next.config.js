4e901e4b1e54f735549864a71d00f67ff00b4922
Hen-Heang <henheang15@gmail.com>
2026-03-18 09:06:10 +0900

    Remove experimental turbopack flag

--- next.config.js
+++ next.config.js
@@ -3,9 +3,6 @@ const nextConfig = {
     typescript: {
         ignoreBuildErrors: true,
     },
-    experimental: {
-        turbopackUseSystemTlsCerts: true,
-    },
 };
 
 module.exports = nextConfig;
